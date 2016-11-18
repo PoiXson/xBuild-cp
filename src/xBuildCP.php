3c318bc82318dd382846e1a8042cfbaf4ef5db49
lorenzop <lorenzo@poixson.com>
2016-11-18 18:54:13 -0500

    added xBuildCP and xBuildShell empty class files

---
+++ src/xBuildCP.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * xBuild Control Panel
+ * @copyright 2016
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\xBuildCP;
+
+
+class xBuildCP extends \pxn\phpPortal\WebApp {
+
+
+}
